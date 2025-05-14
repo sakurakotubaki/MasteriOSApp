d7650bd752d2d95fd2dd903fc6edebc41328d3c9
sakurakotubaki <gitonly543@gmail.com>
2025-05-15 07:31:50 +0900

    Initial Commit

---
+++ MasteriOSApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MasteriOSApp
+//
+//  Created by 橋本純一 on 2025/05/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MasteriOSApp/MasteriOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasteriOSAppApp.swift
+//  MasteriOSApp
+//
+//  Created by 橋本純一 on 2025/05/15.
+//
+
+import SwiftUI
+
+@main
+struct MasteriOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
