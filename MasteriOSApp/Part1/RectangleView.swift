00ca0022bad5df833d8f2f72c047366b7897244d
sakurakotubaki <gitonly543@gmail.com>
2025-05-15 08:21:05 +0900

    style: Rectangle Example

---
+++ MasteriOSApp/Part1/RectangleView.swift
@@ -0,0 +1,18 @@
+//
+//  RectangleView.swift
+//  MasteriOSApp
+//
+//  Created by 橋本純一 on 2025/05/15.
+//
+
+import SwiftUI
+
+struct RectangleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RectangleView()
+}
