3d7be1bd6cd334730023fe41f16495496ba56d8a
sakurakotubaki <gitonly543@gmail.com>
2025-05-15 08:08:47 +0900

    style: image size

---
+++ MasteriOSApp/Part1/ImageView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageView.swift
+//  MasteriOSApp
+//
+//  Created by 橋本純一 on 2025/05/15.
+//
+
+import SwiftUI
+
+struct ImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageView()
+}
